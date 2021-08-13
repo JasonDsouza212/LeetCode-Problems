eac88ef47e6c5982d780818aa3f9d87fce641c71
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2021-08-14 01:50:55 +0800

    Time: 0 ms (100.00%), Space: 39.1 MB (68.78%) - LeetHub

---
+++ shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[]=new int[2*n];
+        int start=1;
+        int m=n;
+        int k=1;
+        ans[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(i%2==0){
+                ans[i]=nums[k];
+                k++;
+            }else{
+                ans[i]=nums[m];
+                m++;
+            }
+        }
+        return ans;
+    }
+}
