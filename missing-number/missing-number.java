060b5e5baecbd1fb5520fb641961b979612368c5
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2021-10-14 17:11:08 +0530

    Time: 1 ms (60.72%), Space: 39.3 MB (63.29%) - LeetHub

---
+++ missing-number/missing-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       return sort(nums); 
+    }
+    public int sort(int arr[]){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i];
+            if(arr[i]<arr.length && arr[i]!=arr[correct]){
+                int temp=arr[i];
+                arr[i]=arr[correct];
+                arr[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]==j){
+                continue;
+            }else{
+                return j;
+            }
+        }
+        return arr.length;
+    }
+}
