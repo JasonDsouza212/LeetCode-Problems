4ae474ad774dfc40a5e6f98629a06173602f64cd
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2021-10-16 13:42:32 +0530

    Time: 4 ms (45.28%), Space: 52.2 MB (8.93%) - LeetHub

---
+++ set-mismatch/set-mismatch.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+     int ans[]=new int[2];
+      int i=0;
+        while(i<nums.length){
+           
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                int temp=nums[i];
+                nums[i]=nums[correct];
+                nums[correct]=temp;
+            }
+            else{
+                i++;
+            }
+    }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1){
+                ans[0]=nums[j];
+                ans[1]=j+1;
+            }
+        }
+        return ans;
+       
+}
+}
